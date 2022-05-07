840a6e97a6b8b35846710e5c22562b0000ece78c
Kanor Ultimatum <99815386+KanorLegacy@users.noreply.github.com>
2022-05-08 02:20:41 +0800

    Delete __pycache__ directory

--- __pycache__/ev_script_2_stat.py
+++
@@ -1,6 +0,0 @@
-import math
-class Class_EV():
-
-    def desired(base,evv,ivv,opt,lvl,EV_Desired_Choice,mod):
-        Total_EV_Desired = ((2*base[opt+1]+ivv[opt+1]+(evv[opt+1]/4))*lvl/100)
-        return math.trunc((((EV_Desired_Choice/mod)-(Total_EV_Desired))*400/lvl)/4)*4
